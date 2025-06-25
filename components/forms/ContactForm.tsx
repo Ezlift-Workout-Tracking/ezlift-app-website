d85b3cf7fa83162a1f64b33bd829178828765279
Fahmy Farahat <fahmy.farahat@gmail.com>
2025-06-25 17:45:27 +0200

    remove header

--- components/forms/ContactForm.tsx
+++ components/forms/ContactForm.tsx
@@ -4,24 +4,11 @@ import type React from "react";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
-import { Send } from "lucide-react";
 
 export function ContactForm() {
   return (
     <Card className="w-full max-w-4xl mx-auto">
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Send className="h-5 w-5" />
-          Contact Us
-        </CardTitle>
-        <CardDescription>
-          Send us a message and we'll get back to you.
-        </CardDescription>
-      </CardHeader>
       <CardContent>
         <div className="w-full overflow-hidden rounded-lg border">
           <iframe 
