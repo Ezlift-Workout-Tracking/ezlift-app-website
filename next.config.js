a5093d4182bd2779cd59dff55eeed20827c75eee
Fahmy Farahat <fahmy.farahat@gmail.com>
2025-08-04 23:32:08 +0200

    fix build (#12)
    * fix build
    
    * add sitemap
    
    * update config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }
+  images: { unoptimized: true },
+    env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  }
 };
 
 module.exports = nextConfig;
